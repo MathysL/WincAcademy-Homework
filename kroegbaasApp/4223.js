995ff1f2b7e344b0135dbeb78f651d2a1a62c637
Mathijs <libro@live.nl>
2021-07-29 16:19:33 +0200

    if else javascript opdracht kroegbaas opdracht
    3 scripts in 1 ;4223 is id opdracht

---
+++ kroegbaasApp/4223.js
@@ -0,0 +1,20 @@
+const age = Number;
+if (age >= 18) {
+    console.log("Hello, you may enter");
+} else {
+    console.log("No, you are too young, please wait outside");
+}
+
+const isFemale = false;
+if (isFemale) {
+    console.log("yes you may order-online, Welcome ..' + isFemale");
+} else {
+    console.log("no tickets for Men today , 'go now and come backanotherday ;-)'");
+}
+
+const driverStatus = "not bob";
+if (driverStatus == "bob") {
+    console.log("Hello Bob, heres the keychain, it is safe for you to drive, find a non Bob");
+} else {
+    console.log(" Stop and find 'a Bob' it's not safe for you to drive");
+}
