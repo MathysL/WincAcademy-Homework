7ebb3f3eff178ed19ee5b95bb8b831fb735fefc8
Mathijs <libro@live.nl>
2021-07-29 17:10:08 +0200

    opdracht kroegbaas App V2
    id 4227: update met calculatie en jubuleum actie

---
+++ kroegbaasApp/4227.js
@@ -0,0 +1,43 @@
+const firstName = ('anyName');
+if firstName = "Sara"||"Abraham" {
+    console.log("Hello," + firstName " and welcome 'you get a: gratis biertje op deze feestelijke dag.' and may enter");
+} else {
+    console.log("No, you are still too young, please wait your turn");
+}
+const korting = 50 %;
+const totalAmount = (0, 00);
+const total = ("Euro: " + (totalAmount +((x)*2)-korting);
+const x = aantal;
+if (x < 1) {
+    consol.log('de rekening is' + total "zonder korting van 50% " + (x * 1) 'biertjes');
+} else {
+    consol.log('de rekening is' + total "met korting van 50% " + (x * 1) 'biertjes');
+}
+const age = Number;
+if ((age >= 18)) {
+    console.log("Hello, you may enter");
+} else {
+    console.log("No, you are too young, please wait outside");
+}
+if (age (18 < 25)) {
+    console.log("Je krijgt 50% korting!");
+}
+if (age = 50) {
+    console.log("1 gratis biertje op deze feestelijke dag");
+} else {
+    console.log("Not for you are too young");
+}
+
+const isFemale = false;
+if (isFemale) {
+    console.log("yes you may order-online, Welcome ..' + isFemale");
+} else {
+    console.log("no tickets for Men today , 'go now and come backanotherday ;-)'");
+}
+
+const driverStatus = "not bob";
+if (driverStatus == "bob") {
+    console.log("Hello Bob, heres the keychain, it is safe for you to drive, find a non Bob");
+} else {
+    console.log(" Stop and find 'a Bob' it's not safe for you to drive");
+}
