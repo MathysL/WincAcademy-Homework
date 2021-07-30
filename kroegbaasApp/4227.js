c765ec0c1b8d12c66cc438c80304b15a1f29823d
Mathijs <libro@live.nl>
2021-07-30 12:47:15 +0200

    update-4227.js
    +functies , niet getest) en laatste versie  van de 2de opdracht)

--- kroegbaasApp/4227.js
+++ kroegbaasApp/4227.js
@@ -1,24 +1,38 @@
-const firstName = ('anyName');
-if firstName = "Sara"||"Abraham" {
-    console.log("Hello," + firstName " and welcome 'you get a: gratis biertje op deze feestelijke dag.' and may enter");
+const age = Number;
+const korting = 50%;
+const float totalAmount = (0,00);
+const total = ("Euro: " + (totalAmount +((aantal)*2)-korting);
+const firstName = "anyName";
+
+if firstName === "Sara" || firstName === "Abraham" {
+    console.log("Hello," + firstName "'and welcome you get a: gratis biertje op deze feestelijke dag.' and may enter");
 } else {
     console.log("No, you are still too young, please wait your turn");
 }
-const korting = 50 %;
-const totalAmount = (0, 00);
-const total = ("Euro: " + (totalAmount +((x)*2)-korting);
-const x = aantal;
-if (x < 1) {
+// in biertjes
+if (aantal < 1) {
     consol.log('de rekening is' + total "zonder korting van 50% " + (x * 1) 'biertjes');
 } else {
     consol.log('de rekening is' + total "met korting van 50% " + (x * 1) 'biertjes');
 }
-const age = Number;
-if ((age >= 18)) {
+//in $
+total = aantal;
+aantal = 1;
+if (totalAmount >= 100) {
+    console.log("gratis flesje champagne.");
+} else if (totalAmount > 50) {
+    console.log("gratis portie nachos");
+} else if (totalAmount > 25) {
+    console.log("gratis (vega)bitterballen");
+} else {
+    console.log("Helaas geen korting, maar leuk dat je er bent");
+}
+if (age >= 18) {
     console.log("Hello, you may enter");
 } else {
     console.log("No, you are too young, please wait outside");
 }
+
 if (age (18 < 25)) {
     console.log("Je krijgt 50% korting!");
 }
@@ -30,13 +44,13 @@ if (age = 50) {
 
 const isFemale = false;
 if (isFemale) {
-    console.log("yes you may order-online, Welcome ..' + isFemale");
+    console.log("yes you may order-online, Welcome .." + isFemale);
 } else {
     console.log("no tickets for Men today , 'go now and come backanotherday ;-)'");
 }
 
 const driverStatus = "not bob";
-if (driverStatus == "bob") {
+if (driverStatus === "bob") {
     console.log("Hello Bob, heres the keychain, it is safe for you to drive, find a non Bob");
 } else {
     console.log(" Stop and find 'a Bob' it's not safe for you to drive");
