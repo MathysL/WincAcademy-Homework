ac4578b22b7e66f215c3594f940b15de7a28177b
Mathijs <libro@live.nl>
2021-08-03 17:59:43 +0200

    pizza sushi opdracht

---
+++ keukenScript/keuken.js
@@ -0,0 +1,28 @@
+const makeSushi = function () {
+    console.log("Maak zelf Sushi||:");
+    console.log("Stap 1: Sushirijst koken");
+    console.log("Meng de Sushirijst met de sushi azijn");
+    console.log("slice the raw fish");
+    console.log("Snij nadat je de sushirijst hebt gekookt eerst de ingrediënten die je hiervoor hebt uitgekozen in kleine blokjes/stukjes. Dit kan vis, gekookte groente, komkommer, avocado of iets anders zijn.");
+    console.log("Sushirijst verspreiden over sushimatje");
+    console.log("Sushi vullen met lekkere producten, boven op de rijst");
+    console.log("Sushi uitrollen tot een ongesneden rol ");
+    console.log("De sushi snijden in stukken en klaar!");
+    console.log("O O O O O O // O O      ");
+};
+//
+makeSushi();
+//
+const makePizza = function () {
+  console.log("Maak een pizza, met wat water, een kop meel en beetje olijf olie");
+  console.log("Doe de ingrediënten voor de bodem in een mengkom, en maak het deeg");
+  console.log("laat het deeg rijzen een uur en roll het uit tot een dunne ronde lap deeg, van 25CM.");
+  console.log("zet de oven alvast aan en op een temperatuur van 250 graden celcius");
+  console.log("smeer een laag tomatensaus, gevolgd door een dun laagje kaas geraspt en vervolgens de toppings naar keuze met op het laatst weer een laag geraspte kaas");
+  console.log("Doe de pizza in de oven (220 tot 250C)");
+    console.log("na 20 minuten is de pizza klaar en mag hij uit de oven");
+  console.log("Pizza time");
+};
+//
+makePizza();
+//
