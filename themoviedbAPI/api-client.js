5c0f842bb30b7892388294fcf5290cf731934f58
Mathijs <libro@live.nl>
2021-12-03 14:46:27 +0100

    TMDb rating App Async Await
    themovieDb index.html: via Async Await script en  api-client

--- themoviedbAPI/api-client.js
+++ themoviedbAPI/api-client.js
@@ -1,29 +1,97 @@
-//1 ini const
+const api_key = '21fe51973186dd156bd8da32ca25498f';
+const API_KEY = api_key;
+const apiUrl = "https://api.themoviedb.org/3";
 const apiUrl_genre = 'https://api.themoviedb.org/3/genre/movie/list';
-const apiUrl = 'https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>';
-const API_KEY = 'api_key='+ myApi_key;
-const api_key = '<<api_key>>';
-const myApi_key = '000000000000000000000000007'
 // api_key invullen op deze plaats of later bearer token info
-    const ACCES_TOKEN = access_token;
-    const bearerurl= 'https://api.themoviedb.org/3/movie/76341'
-//2 logic
-async function getData()
-    try {
-    // 'adresnaam' of locatie +_genre
-        const response = await makeRequest('https://api.themoviedb.org/3/movie/76341' + API_KEY)
-    console.log("response Received")
-    const proccessResponse = await processRequest()
-    console.log(proccessResponse)
-} catch (err) {
-    console.log(err)
-}
+//
+const res = async function getData() {
+    const apiUrlData = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    try {// 'adresnaam' of locatie +_genre
+        const response = await makeRequest('https://api.themoviedb.org/3/movie/76341' + API_KEY);
+        console.log("response Received");
+        const proccessResponse = await processRequest();
+        console.log(proccessResponse);
+    } catch (err) {
+        console.log(err);
+    }
     //input
-getData()
-// copy to output
-fetch("https://api.themoviedb.org/3/movie/76341")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => {
-        console.log(error)
-    });
+    res();
+    // copy to output
+    fetch("https://api.themoviedb.org/3/movie/76341")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => {
+            console.log(error)
+        });
+ }   
+//
+const TMDb_id = "id";
+const Title = "name";
+const getData = async () => {
+    const apiUrlList = `${apiUrl}/genre/movie/list?api_key=${API_KEY}`;
+    try {
+        const res = await fetch(apiUrlList, { method: "GET" });
+        const data = await res.json();
+        console.log("The data in getData function ", data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+//
+
+
+//step 1
+const getMovieGenres = async () => {
+    //getMovie Genres
+    const APIEndpoint = `${apiUrl}/genre/movie/list?api_key=${API_KEY}`;
+    try {
+        const res = await fetch(APIEndpoint, { method: "GET" });
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getMovieById = async movieId => {
+    // api.themoviedb.org/3/find/.. //movieId
+    const movieByIDEndpoint = `${apiUrl}/find/${movieId}?external_source=TMDb_id&api_key=${API_KEY}`;
+    try {
+        const res = await fetch(movieByIDEndpoint, { method: "GET" });
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getTopRatedMovies = async () => {
+    // movie/top_rated
+    const topRatedMoviesEndpoint = `${apiUrl}/movie/top_rated?api_key=${API_KEY}`;
+    try {
+        const res = await fetch(topRatedMoviesEndpoint, { method: "GET" });
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getTopRatedMoviesFromYearX = async year => {
+    // discover/movie?primary_release_year=2010&sort_by=vote_average.desc
+    const topRatedMoviesEndpoint = `${apiUrl}/discover/movie?primary_release_year=${year}&sort_by=vote_average.desc&vote_count.gte=2000&api_key=${API_KEY}`;
+    try {
+        const res = await fetch(topRatedMoviesEndpoint, { method: "GET" });
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getTopRatedMoviesInGenreId = async genreId => {
+    const topRatedMoviesEndpoint = `${apiUrl}/discover/movie?with_genres=${genreId}&sort_by=vote_average.desc&vote_count.gte=2000&api_key=${API_KEY}`;
+    try {
+        const res = await fetch(topRatedMoviesEndpoint, { method: "GET" });
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
--- themoviedbAPI/script.js
+++ themoviedbAPI/script.js
@@ -1,7 +1,95 @@
-//// JavaScript source code
-fetch("https://swapi.dev/api/people/1/")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => {
-        console.log(error)
+const getGenreMovies = 'https://api.themoviedb.org/3/movie/550?api_key=21fe51973186dd156bd8da32ca25498f&/genre/movie/list=/genre/movie/list';
+//vb(name en) id alleen zoeken
+//test id  zoek online of in response.json
+const LION_KING_IMDBID = "tt6105098";
+const The_Joker_2019 = "12";// 
+const Toy_Story_4 = "120";// 
+
+const setMovieGenres = async () => {
+    const movieGenresList = document.getElementById("movieGenresList");
+    const data = await getMovieGenres();
+    data.genres.forEach(movieGenre => {
+        const li = document.createElement("li");
+        const liContent = document.createTextNode(
+            `genre naam: ${movieGenre.name}, id: ${movieGenre.id}`
+        );
+        li.appendChild(liContent);
+        movieGenresList.append(li);
     });
+};
+
+const setSingleMovie = async () => {
+    const data = await getMovieById(LION_KING_IMDBID);
+    const movie = data.movie_results[0];
+    const topMovieParagraph = document.getElementById("myTopMovie");
+    topMovieParagraph.appendChild(document.createTextNode(movie.title));
+};
+
+const setTopRatedMovies = async () => {
+    const topRatedList = document.getElementById("topRated");
+    const data = await getTopRatedMovies();
+    const topTen = data.results.splice(0, 10);
+    topTen.forEach(movie => {
+        const li = document.createElement("li");
+        const liContent = document.createTextNode(
+            `${movie.title}, rating: ${movie.vote_average}`
+        );
+        li.appendChild(liContent);
+        topRatedList.append(li);
+    });
+};
+
+const setTopRatedMoviesForYear = async year => {
+    const topRatedList = document.getElementById("topRatedFromYear");
+    topRatedList.innerHTML = "";
+    const data = await getTopRatedMoviesFromYearX(year);
+    const topTen = data.results.splice(0, 10);
+    topTen.forEach(movie => {
+        const li = document.createElement("li");
+        const liContent = document.createTextNode(
+            `${movie.title}, rating: ${movie.vote_average}, vote count: ${movie.vote_count}`
+        );
+        li.appendChild(liContent);
+        topRatedList.append(li);
+    });
+};
+
+const setTopRatedMoviesInGenre = async genreId => {
+    const topRatedList = document.getElementById("topInGenre");
+    topRatedList.innerHTML = "";
+    const data = await getTopRatedMoviesInGenreId(genreId);
+    const topTen = data.results.splice(0, 10);
+    topTen.forEach(movie => {
+        const li = document.createElement("li");
+        const liContent = document.createTextNode(
+            `${movie.title}, rating: ${movie.vote_average}, vote count: ${movie.vote_count}`
+        );
+        li.appendChild(liContent);
+        topRatedList.append(li);
+    });
+};
+
+const addTopMoviesForYearXListener = () => {
+    const button = document.getElementById("topRatedFromYearButton");
+    button.addEventListener("click", () => {
+        const input = document.getElementById("topRatedFromYearInput");
+        const year = input.value;
+        setTopRatedMoviesForYear(year);
+    });
+};
+
+const addTopMoviesForGenreXListener = () => {
+    const button = document.getElementById("topInGenreButton");
+    button.addEventListener("click", () => {
+        const genreId = document.getElementById("topInGenreInput").value;
+        setTopRatedMoviesInGenre(genreId);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    setSingleMovie();
+    setMovieGenres();
+    setTopRatedMovies();
+    addTopMoviesForYearXListener();
+    addTopMoviesForGenreXListener();
+});
